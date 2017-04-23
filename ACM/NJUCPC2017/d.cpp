31053cc9353b0149d3373f520862677d87ffa934
Kestory <kestory@163.com>
2017-04-23 20:04:45 +0800

    tiny changes

--- ACM/NJUCPC2017/d.cpp
+++ ACM/NJUCPC2017/d.cpp
@@ -2,59 +2,59 @@
 #include <string.h>
 #include <cmath>     /* fabs&&abs */
 #include <float.h>
-#include<iomanip>	
+#include<iomanip>
 #define D(x) cout<<#x<<"="<<x<<'\t';
 using namespace std;
 
-double find_m(double *a,int n){//get the max fabs
-	double z=-1;
-	for(int i=0;i<n;++i)
-		if(fabs(a[i])>z)
-			z=fabs(a[i]);
-	return z;		
+double find_m(double *a, int n) { //get the max fabs
+	double z = -1;
+	for (int i = 0; i < n; ++i)
+		if (fabs(a[i]) > z)
+			z = fabs(a[i]);
+	return z;
 }
-double find_max(double *a,int n){//get the max
-	double z=DBL_MIN;
-	for(int i=0;i<n;++i)
-		if(a[i]>z)
-			z=a[i];
-	return z;		
+double find_max(double *a, int n) { //get the max
+	double z = DBL_MIN;
+	for (int i = 0; i < n; ++i)
+		if (a[i] > z)
+			z = a[i];
+	return z;
 }
-double find_min(double *a,int n){//get the min
-	double z=DBL_MAX;
-	for(int i=0;i<n;++i)
-		if(a[i]<z)
-			z=a[i];
-	return z;		
+double find_min(double *a, int n) { //get the min
+	double z = DBL_MAX;
+	for (int i = 0; i < n; ++i)
+		if (a[i] < z)
+			z = a[i];
+	return z;
 }
-int main(){
+int main() {
 	ios::sync_with_stdio(false);
-	int t=0;
-	cin>>t;
+	int t = 0;
+	cin >> t;
 
-	for(int i=0;i<t;i++){
+	for (int i = 0; i < t; i++) {
 		int n;
-		cin>>n;
-		double *a=new double [n];
-		double *b=new double [n];
-		for(int j=0;j<n;j++)
-			cin>>a[j]>>b[j];
-		double f=find_m(a,n);
-		double mm=find_max(a,n)-find_min(a,n);
-		for(int k=0;k<f;k++){
-			
-			for(int w=0;w<n;w++)	a[w]+=b[w];
-			double mmm=fabs(find_max(a,n)-find_min(a,n));
+		cin >> n;
+		double *a = new double [n];
+		double *b = new double [n];
+		for (int j = 0; j < n; j++)
+			cin >> a[j] >> b[j];
+		double f = find_m(a, n);
+		double mm = find_max(a, n) - find_min(a, n);
+		for (int k = 0; k < f; k++) {
 
-		//D(find_max(a,n))D(find_min(a,n))
-			if(mmm<mm)
-				mm=mmm;
+			for (int w = 0; w < n; w++)	a[w] += b[w];
+			double mmm = fabs(find_max(a, n) - find_min(a, n));
+
+			//D(find_max(a,n))D(find_min(a,n))
+			if (mmm < mm)
+				mm = mmm;
 		}
 
 		//delete []a;
 		//delete []b;
 
-		cout<<setiosflags(ios::fixed)<<setprecision(7)<<mm<<endl;
+		cout << setiosflags(ios::fixed) << setprecision(7) << mm << endl;
 
 	}
 	return 0;
